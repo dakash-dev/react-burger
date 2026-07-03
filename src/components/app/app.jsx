e96c574c82c45e2e955574bdfa1972394a03f29a
Dmitry Kashirin <urdino@yandex.ru>
2026-07-03 17:06:56 +0300

    Добавлен preloader

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,50 @@
+import { useState, useEffect } from 'react';
+
+import Preloader from '@/components/preloader/preloader';
+import { BASE_URL } from '@/utils/constants';
 import { AppHeader } from '@components/app-header/app-header';
 import { BurgerConstructor } from '@components/burger-constructor/burger-constructor';
 import { BurgerIngredients } from '@components/burger-ingredients/burger-ingredients';
-import { ingredients } from '@utils/ingredients';
 
 import styles from './app.module.css';
 
+// Конструкция получена по документации от ИИ.
 export const App = () => {
+  // Сптсок инградиентов, получаемые с сервера.
+  const [ingredientsData, setIngredientsData] = useState([]);
+  // Состояние загрузки.
+  const [isLoading, setIsLoading] = useState(true);
+  // Вероятные ошибки.
+  const [hasError, setHasError] = useState(null);
+
+  // Получение данных с сервера.
+  const getIngredients = async () => {
+    try {
+      const responce = await fetch(`${BASE_URL}/ingredients`);
+      if (!responce.ok) {
+        throw new Error('Invalid request');
+      }
+      const fetchData = await responce.json();
+      setIngredientsData(fetchData.data);
+    } catch (error) {
+      setHasError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getIngredients();
+  }, []);
+
+  if (isLoading) {
+    // return <div>Загрузка космических ингредиентов...</div>;
+    return <Preloader />;
+  }
+  if (hasError) {
+    return <div>Ошибка загрузки</div>;
+  }
+
   return (
     <div className={styles.app}>
       <AppHeader />
@@ -13,8 +52,10 @@ export const App = () => {
         Соберите бургер
       </h1>
       <main className={`${styles.main} pl-5 pr-5`}>
-        <BurgerIngredients ingredients={ingredients} />
-        <BurgerConstructor ingredients={ingredients} />
+        {/* <BurgerIngredients ingredients={ingredients} /> */}
+        {/* <BurgerConstructor ingredients={ingredients} /> */}
+        <BurgerIngredients ingredients={ingredientsData} />
+        <BurgerConstructor ingredients={ingredientsData} />
       </main>
     </div>
   );
