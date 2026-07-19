67dc79fd497c97e95f4d1f2e47ed61af75c4cf5c
Dmitry Kashirin <urdino@yandex.ru>
2026-07-20 02:16:38 +0300

    feat: подключен редакс к конструктору и добавлены заглушки

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -4,76 +4,101 @@ import {
   Button,
   DragIcon,
 } from '@krgaa/react-developer-burger-ui-components';
+import { useSelector } from 'react-redux';
 
 import styles from './burger-constructor.module.css';
 
-export const BurgerConstructor = ({ ingredients, onOrderClick }) => {
-  console.log(ingredients);
-
-  const bun = ingredients.find((ingredient) => ingredient.type === 'bun');
-  const ingredientsWithoutBun = ingredients.filter(
-    (ingredient) => ingredient.type !== 'bun'
+export const BurgerConstructor = ({ onOrderClick }) => {
+  const { bun, ingredients: constructorIngredients } = useSelector(
+    (state) => state.burgerConstructor
   );
+
+  console.log('Данные конструктора из Redux:', { bun, constructorIngredients });
 
   return (
     <section className={styles.burger_constructor}>
-      {bun && (
-        <>
-          <div className={`${styles.burger_list} pl-4`}>
-            {/*  Верх списка инградиентов */}
-            <ConstructorElement
-              type="top"
-              isLocked={true}
-              text={`${bun.name} (верх)`}
-              price={bun?.price}
-              thumbnail={bun?.image}
-              extraClass="ml-8"
-            />
-            {/*  Список инградиентов со скроллом*/}
-            <ul className={`${styles.ingredients_set} custom-scroll`}>
-              {ingredientsWithoutBun.map((base) => (
-                <li key={base._id} className={styles.ingredients_base}>
-                  <DragIcon type="primary" />
-                  <ConstructorElement
-                    text={base.name}
-                    price={base.price}
-                    thumbnail={base.image}
-                    extraClass="ml-2"
-                  />
-                </li>
-              ))}
-            </ul>
-            {/*  Низ списка инградиентов */}
-            <ConstructorElement
-              type="bottom"
-              isLocked={true}
-              text={`${bun.name} (низ)`}
-              price={bun?.price}
-              thumbnail={bun?.image}
-              extraClass="ml-8"
-            />
+      <div className={`${styles.burger_list} pl-4`}>
+        {/* Верхняя булка или заглушка */}
+        {bun ? (
+          <ConstructorElement
+            type="top"
+            isLocked={true}
+            text={`${bun.name} (верх)`}
+            price={bun.price}
+            thumbnail={bun.image}
+            extraClass="ml-8"
+          />
+        ) : (
+          <div
+            className={`${styles.empty_bun_top} ml-8 text text_type_main-default text_color_inactive`}
+          >
+            Выберите булки
           </div>
-          {/*  Итогая стоимость. */}
-          <div className={`${styles.place_order} mt-10`}>
-            <div className={`${styles.price_order} pr-6`}>
-              <span className="text text_type_digits-medium mr-2">610</span>
-              <span className={`${styles.currency_icon} mr-10`}>
-                <CurrencyIcon type="primary" />
-              </span>
-              <span>
-                <Button
-                  htmlType="button"
-                  type="primary"
-                  size="large"
-                  onClick={onOrderClick}
-                >
-                  Оформить заказ
-                </Button>
-              </span>
-            </div>
+        )}
+
+        {/* Список начинок или заглушка */}
+        {constructorIngredients.length > 0 ? (
+          <ul className={`${styles.ingredients_set} custom-scroll`}>
+            {constructorIngredients.map((base) => (
+              // Используем уникальный id из nanoid в качестве ключа
+              <li key={base.id} className={styles.ingredients_base}>
+                <DragIcon type="primary" />
+                <ConstructorElement
+                  text={base.name}
+                  price={base.price}
+                  thumbnail={base.image}
+                  extraClass="ml-2"
+                />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div
+            className={`${styles.empty_ingredients} ml-8 text text_type_main-default text_color_inactive`}
+          >
+            Выберите начинки и соусы
           </div>
-        </>
-      )}
+        )}
+
+        {/* Нижняя булка или заглушка */}
+        {bun ? (
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text={`${bun.name} (низ)`}
+            price={bun.price}
+            thumbnail={bun.image}
+            extraClass="ml-8"
+          />
+        ) : (
+          <div
+            className={`${styles.empty_bun_bottom} ml-8 text text_type_main-default text_color_inactive`}
+          >
+            Выберите булки
+          </div>
+        )}
+      </div>
+
+      {/* Блок стоимости и кнопка заказа */}
+      <div className={`${styles.place_order} mt-10`}>
+        <div className={`${styles.price_order} pr-6`}>
+          <span className="text text_type_digits-medium mr-2">0</span>
+          <span className={`${styles.currency_icon} mr-10`}>
+            <CurrencyIcon type="primary" />
+          </span>
+          <span>
+            <Button
+              htmlType="button"
+              type="primary"
+              size="large"
+              onClick={onOrderClick}
+              disabled={!bun} // Кнопка заблокирована, пока не выполним перетаскивание инградиента.
+            >
+              Оформить заказ
+            </Button>
+          </span>
+        </div>
+      </div>
     </section>
   );
 };
