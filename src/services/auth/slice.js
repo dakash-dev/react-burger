9199e9fd10d51b3bd94caa1cb3f9a297c5f235a3
Dmitry Kashirin <urdino@yandex.ru>
2026-08-04 00:41:11 +0300

    feat: добавил actions loginUser и logoutUser в authSlice

---
+++ src/services/auth/slice.js
@@ -0,0 +1,100 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+import {
+  loginUserRequest,
+  setTokens,
+  logoutUserRequest,
+  clearTokens,
+} from '@/utils/burger-api';
+
+// Асинхронный экшен для входа в систему
+export const loginUser = createAsyncThunk(
+  'auth/login',
+  async (form, { rejectWithValue }) => {
+    try {
+      const data = await loginUserRequest(form);
+      if (!data.success) {
+        return rejectWithValue(data);
+      }
+      // Сохраняем токены в localStorage при успешном входе
+      setTokens(data.accessToken, data.refreshToken);
+      // Возвращаем данные пользователя { user: { email, name } }
+      return data.user;
+    } catch (err) {
+      return rejectWithValue(err.message || 'Ошибка авторизации');
+    }
+  }
+);
+
+// Асинхронный экшен для выхода из системы
+export const logoutUser = createAsyncThunk(
+  'auth/logout',
+  async (_, { rejectWithValue }) => {
+    try {
+      const data = await logoutUserRequest();
+      if (!data.success) {
+        return rejectWithValue(data);
+      }
+      // При успешном ответе сервера полностью очищаем localStorage
+      clearTokens();
+      return data;
+    } catch (err) {
+      return rejectWithValue(err.message || 'Ошибка при выходе из системы');
+    }
+  }
+);
+
+const initialState = {
+  user: null, // Данные пользователя: { email: '', name: '' }
+  isAuthChecked: false, // Флаг: завершилась ли проверка токена при старте приложения
+  isLoading: false, // Флаг загрузки для сетевых запросов авторизации
+  error: null, // Текст ошибки, если запрос упал.
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    // Сеттер для ручной установки флага проверки авторизации
+    setAuthChecked: (state, action) => {
+      state.isAuthChecked = action.payload;
+    },
+    // Сеттер для установки данных пользователя
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      //login
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+        state.isAuthChecked = true;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      // logout
+      .addCase(logoutUser.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(logoutUser.fulfilled, (state) => {
+        state.isLoading = false;
+        state.user = null;
+      })
+      .addCase(logoutUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { setAuthChecked, setUser } = authSlice.actions;
+export default authSlice.reducer;
