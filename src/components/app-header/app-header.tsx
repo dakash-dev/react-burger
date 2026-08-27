5d997c0a61a1522d6a9281b4abc294c4ede48a88
Dmitry Kashirin <urdino@yandex.ru>
2026-08-27 03:23:17 +0300

    refactor: перевод компонента AppHeader на TypeScript

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -6,20 +6,22 @@ import {
 } from '@krgaa/react-developer-burger-ui-components';
 import { NavLink, Link } from 'react-router-dom';
 
+import type { ReactElement } from 'react';
+
 import styles from './app-header.module.css';
 
-export const AppHeader = () => {
+export const AppHeader = (): ReactElement => {
   return (
     <header className={styles.header}>
       <nav className={`${styles.menu} p-4`}>
         <div className={styles.menu_part_left}>
           <NavLink
             to="/"
-            className={({ isActive }) =>
+            className={({ isActive }: { isActive: boolean }): string =>
               `${styles.link} ${isActive ? styles.link_active : ''}`
             }
           >
-            {({ isActive }) => (
+            {({ isActive }: { isActive: boolean }): ReactElement => (
               <>
                 <BurgerIcon type={isActive ? 'primary' : 'secondary'} />
                 <p className="text text_type_main-default ml-2">Конструктор</p>
@@ -28,11 +30,11 @@ export const AppHeader = () => {
           </NavLink>
           <NavLink
             to="/feed"
-            className={({ isActive }) =>
+            className={({ isActive }: { isActive: boolean }): string =>
               `${styles.link} ml-10 ${isActive ? styles.link_active : ''}`
             }
           >
-            {({ isActive }) => (
+            {({ isActive }: { isActive: boolean }): ReactElement => (
               <>
                 <ListIcon type={isActive ? 'primary' : 'secondary'} />
                 <p className="text text_type_main-default ml-2">Лента заказов</p>
@@ -45,11 +47,11 @@ export const AppHeader = () => {
         </Link>
         <NavLink
           to="/profile"
-          className={({ isActive }) =>
+          className={({ isActive }: { isActive: boolean }): string =>
             `${styles.link} ${styles.link_position_last} ${isActive ? styles.link_active : ''}`
           }
         >
-          {({ isActive }) => (
+          {({ isActive }: { isActive: boolean }): ReactElement => (
             <>
               <ProfileIcon type={isActive ? 'primary' : 'secondary'} />
               <p className="text text_type_main-default ml-2">Личный кабинет</p>
